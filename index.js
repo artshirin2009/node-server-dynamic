25f3c72bbf732cc46aba9b159a48071161b11b6a
Arthur Shirin <artshirin2009@gmail.com>
2018-09-19 23:32:00 +0300

    19082018 - 2331

--- index.js
+++ index.js
@@ -16,20 +16,26 @@ app.set('views', path.join(__dirname, 'app/views/templates'))
 
 app.get('/', (request, response) => {
     response.render('home', {
-        user: "Arthur",
-        users: [{
-                name: 'John Doe',
-                age: 30
-            },
-            {
-                name: 'Jane Doy',
-                age: 32
-            }
-        ],
-        helpers: {
-            list: require('./app/helpers/news-item')
-        }
+        user: "Arthur"
     })
+})
+
+app.get('/news', (request, response) => {
+    let news = require('./db.json').news;
+
+    if (request.query.sort.toUpperCase() === 'DESC') {
+        news = news.sort((a, b) => b.id > a.id)
+    }
+    response.render('news/list', {
+        news: require('./db.json').news
+    })
+})
+app.get('/news/:id', (request, response) => {
+    const id = parseInt(request.params.id);
+    response.render('news/item', {
+        news: require('./db.json').news.filter(item => item.id === id)[0]
+    })
+    console.log(require('./db.json').news.filter(item => item.id === id))
 })
 
 app.listen(port, (err) => {
